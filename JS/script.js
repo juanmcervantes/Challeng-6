501b74079f281821c15a9f4622700f8465e01fb1
juanmcervantes <juancervantes0019@gmail.com>
2023-10-12 19:57:59 -0600

    API key added and improved sj and style

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,43 @@
-// Variables for DOM elements
-const searchBtn = document.getElementById('search-btn');
+const searchButton = document.getElementById('search-btn');
 const cityInput = document.getElementById('city-input');
-const searchHistory = document.getElementById('search-history');
-const weatherDisplay = document.getElementById('weather-display');
-const forecast = document.getElementById('forecast');
 
-// Event listener for search button
-searchBtn.addEventListener('click', function() {
-    const city = cityInput.value.trim();
-    if(city) {
-        getWeatherData(city);
+const API_KEY = "b542c1caa308bb6d2314019e5aff33ea";
+
+searchButton.addEventListener('click', () => {
+    const cityName = cityInput.value.trim();
+    if (cityName) {
+        fetchWeather(cityName);
     }
 });
 
-function getWeatherData(city) {
-    // Here you'll fetch data from the API.
-    // Start by fetching coordinates for the city, then use those coordinates to get the weather data.
-    // Render data to the UI.
+function fetchWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+        return response.json();
+    })
+    .then(data => {
+        displayWeather(data);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert('Unable to fetch weather. Please check the city name.');
+    });
 }
 
-function renderWeatherData(data) {
-    // Update DOM elements with the data.
+function displayWeather(data) {
+    const cityNameDateElem = document.getElementById('city-name-date');
+    const currentWeatherIconElem = document.getElementById('current-weather-icon');
+    const currentTempElem = document.getElementById('current-temp');
+    const currentWindElem = document.getElementById('current-wind');
+    const currentHumidityElem = document.getElementById('current-humidity');
+    
+    // Set the data
+    cityNameDateElem.textContent = `${data.name} (${new Date().toLocaleDateString()})`;
+    currentWeatherIconElem.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    currentTempElem.textContent = `Temp: ${data.main.temp}°F`;
+    currentWindElem.textContent = `Wind: ${data.wind.speed} MPH`;
+    currentHumidityElem.textContent = `Humidity: ${data.main.humidity} %`;
 }
-
-function saveToLocalStorage(city) {
-    // Save city to local storage and update search history.
-}
-
-// Load any saved search history from local storage when the page loads.
-window.addEventListener('DOMContentLoaded', function() {
-    // Load search history and render it.
-});
